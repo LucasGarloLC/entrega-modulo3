0ccb5d7489044d90f52e721cbba2bb0d63b17581
Lucas <lgltrad@gmail.com>
2023-04-21 14:42:53 +0200

    Entrega laboratorio 3

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-# typescript-sandbox
+# Entrega del tercer laboratorio.
 
-Para la parte de cada módulo en la que te planteamos ejercicios con JavaScript puedes usar este sandbox.
-
-Pasos:
-
-- Clonate el proyecto.
-- Instala las dependencias con `npm install`.
-- Ejecuta el sandbox con `npm run dev`.
-- Abre el navegador en `http://localhost:5173/` (si ese puerto no te funciona, mira en la consola donde has hecho el build, puede que este ocupado y se haya abierto en otro puerto).
-- Puedes empezar a meter tu código en el fichero:
+La entrega del tercer laboratorio se encuentra en entrega-modulo3>src>main.ts.
--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,71 @@
-/*
-
-- titulo
-- director
-- año
-
-Blade Runner / Ridley Scoot / 1982
-Matrix / Lana Wachowski / 1989
-
-*/
-
-interface Pelicula {
-    titulo: string;
-    director: string;
+interface Grupo {
+    nombre: string;
     año: number;
+    activo: boolean;
+    genero:string;
 };
 
-const peliculaA: Pelicula = {
-    titulo: "Blade Runner",
-    director: "Ridley Scott",
-    año: 1982,
+const style = "font-weight: bold; font-size: 18px; color: green;";
+const poprock = "🎵 Pop Rock";
+const rock = "🎸 Rock";
+const hardrock = "🤘 Hard Rock";
+const clasica = "🎼 Clásica";
+
+const Grupo1 = {
+    nombre: "The Beatles",
+    año:1960,
+    activo: true,
+    genero:poprock,
 };
 
-const peliculaB: Pelicula = {
-    titulo: "Matrix",
-    director: "Lana Wachowski",
-    año: 1999,
+const Grupo2 = {
+    nombre: "Queen",
+    año:1970,
+    activo: false,
+    genero:rock,
 };
 
-console.log (peliculaA);
-console.log (peliculaB);
+const Grupo3 = {
+    nombre: "AC DC",
+    año:1973,
+    activo: true,
+    genero:hardrock,
+};
 
+const Grupo4 = {
+    nombre: "Ludwig van Beethoven",
+    año:1770,
+    activo: false,
+    genero:clasica,
+};
+
+const Grupo5 = {
+    nombre: "The Rolling Stones",
+    año:1962,
+    activo: true,
+    genero:rock,
+};
+console.log("%c"+ Grupo1.nombre, style);
+console.log("- Año de fundación:", Grupo1.año);
+console.log("- Activo:", Grupo1.activo);
+console.log(`- Género musical: ${Grupo1.genero}`);
+
+console.log("%c"+ Grupo2.nombre, style);
+console.log("- Año de fundación:", Grupo2.año)
+console.log("- Activo:", Grupo2.activo)
+console.log(`- Género musical: ${Grupo2.genero}`);
+
+console.log("%c"+ Grupo3.nombre, style);
+console.log("- Año de fundación:", Grupo3.año);
+console.log("- Activo:", Grupo3.activo)
+console.log(`- Género musical: ${Grupo3.genero}`);
+
+console.log("%c"+ Grupo4.nombre, style);
+console.log("- Año de fundación:", Grupo4.año);
+console.log("- Activo:", Grupo4.activo)
+console.log(`- Género musical: ${Grupo4.genero}`);
+
+console.log("%c"+ Grupo5.nombre, style);
+console.log("- Año de fundación:", Grupo5.año)
+console.log("- Activo:", Grupo5.activo)
+console.log(`- Género musical: ${Grupo5.genero}`);
